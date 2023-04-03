d0c9612ae1a7760f048b791a89b4d4b64becb6db
caseyfee <caseyfee3@gmail.com>
2023-04-03 15:05:06 -0700

    minor README and word changes in PRouts

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ This project was created build the back end for an e-commerce site. I used Expre
 
 This project includes a walkthrough video that demonstrates its functionality and all of the following acceptance criteria being met. 
 
+To use it the application, you must use the MySQL2 and Sequelize packages to connect the Express.js API to a MySQL database and the ideally add in a dotenv package to use environment variables to store sensitive data.
+
 ## Table of Contents: 
 - [Description](#description) 
 - [Credits](#credits) 
@@ -20,5 +22,7 @@ Contact me at:
 Email: caseyfee3@gmail.com
 
 ## Video Walkthrough
-
+Tags: https://drive.google.com/file/d/1uJsbqvgfQR0MWLDJBmpf0kRH1zZusvmB/view
+Categories: https://drive.google.com/file/d/1H4vewLFDt8RzaE-2byHgdcHm4HE-geY0/view
+Products: 
     
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,10 +8,10 @@ router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const categories = await Product.findAll({
-      include: [{ model: Category }, {model: Tag}],
+    const productData = await Product.findAll({
+      include: [{ model: Category }, {model: Tag, through: ProductTag}],
     });
-    res.status(200).json(categories);
+    res.status(200).json(productData);
 
   }
   catch (err) {
@@ -24,24 +24,24 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const categories = await Product.findByPk(req.params.id, {
-      include: [{ model: Category }, {model: Tag}],
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, {model: Tag, through: ProductTag}],
     });
-    res.status(200).json(categories);
+    res.status(200).json(productData);
 
-    if (!categories) {
+    if (!productData) {
       res.status(404).json({ message: 'No product found with that id!' });
       return;
     }
 
-    // res.status(200).json(categories);
+    res.status(200).json(categories);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/',  (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -117,14 +117,14 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-    const productData = await Product.destroy({
+    const productData = await Tag.destroy({
       where: {
         id:req.params.id,
       },
     });
     res.status(200).json(productData);
     if (!productData) {
-      res.status(404).json({ message: 'Product deleted!' });
+      res.status(404).json({ message: 'Product delete unsuccessful!' });
       return;
     }
 
